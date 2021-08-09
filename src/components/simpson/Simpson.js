6b06e2483935dafb043e442d54bfa0dfa91ef9d5
learn13579 <maryana.poshta@gmail.com>
2021-08-09 18:05:03 +0300

    commit

---
+++ src/components/simpson/Simpson.js
@@ -0,0 +1,13 @@
+export function Simpson(pTitle, ag, pict, inf) {
+    // console.log(props);
+
+    return (
+        <div className={'simpson'}>
+            <h2>{pTitle}</h2>
+            <h3>{ag}</h3>
+            <img
+                src={pict}
+                alt="foto"/>
+            <p>{inf}</p>
+        </div>);
+}
