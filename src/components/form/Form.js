528e6dbf6208c149601877145b6e2fefee3d8da0
learn13579 <maryana.poshta@gmail.com>
2021-08-17 16:06:27 +0300

    lesson4

---
+++ src/components/form/Form.js
@@ -0,0 +1,57 @@
+import {useState} from "react";
+import {NewCar} from "../../service/servise";
+import Cars from "../cars/Cars";
+import './Form.css'
+
+export function Form() {
+
+    let [model, setModel] = useState('model');
+    let [price, setPrice] = useState('price');
+    let [year, setYear] = useState('year');
+    let [car, setCar] = useState({model: '', price: '', year: ''})
+
+    const onSubmitForm = (e) => {
+        e.preventDefault();
+        let tempCar = {price, model, year}
+        NewCar(tempCar);
+        setCar({...tempCar});
+    }
+
+    const onInputChangeModel = (e) => {
+        let model = e.target.value
+        setModel(model);
+    }
+
+    const onInputChangePrice = (e) => {
+        let price = e.target.value
+        setPrice(price);
+    }
+
+    const onInputChangeYear = (e) => {
+        let year = e.target.value
+        setYear(year);
+    }
+
+    return (
+        <div className={'wrap'}>
+            <h2><b> List of car</b></h2>
+
+            <form onSubmit={onSubmitForm}>
+
+                <input type="text" name={'model'} value={model} onInput={onInputChangeModel}/>
+                <input type="text" name={'price'} value={price} onInput={onInputChangePrice}/>
+                <input type="text" name={'year'} value={year} onInput={onInputChangeYear}/>
+
+                <button>submit</button>
+
+            </form>
+
+            <div>
+                <h3><b>add to the car list:</b></h3>
+                <p>model: {car.model}; price: {car.price}; year: {car.year}</p>
+            </div>
+
+            <div className={'carList'}><Cars/></div>
+        </div>
+    );
+}
