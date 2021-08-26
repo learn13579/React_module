62d15288f2cd0ca773f17d33cead36af64ff0a32
learn13579 <maryana.poshta@gmail.com>
2021-08-26 18:45:52 +0300

    classwork lesson7 switch

--- src/App.js
+++ src/App.js
@@ -7,32 +7,39 @@ import {useReducer} from "react";
 // одна кнопка збільшує значення , інша зменшує
 // логіку реалізувати через reducer
 
-const reduser = (state = {a: 0, b: 0, c: 0}, action) => {
-// console.log(action);
-    if (action.obj === '1' && action.diya === '+') {
-        return {...state, a: state.a + 5};
-    } else if (action.obj === '1' && action.diya === '-') {
-        return {...state, a: state.a - 5};
-    }
+const reduser = (state = {a: 0, b: 0, c: 0}, {obj, count, diya}) => {
 
-    if (action.obj === '2' && action.diya === '+') {
-        return {...state, b: state.b + 10};
-    } else if (action.obj === '2' && action.diya === '-') {
-        return {...state, b: state.b - 10};
+    switch (diya) {
+        case '+':
+            return {...state, [obj]: state[obj] + count}
+        case '-':
+            return {...state, [obj]: state[obj] - count}
+        default:
+            return {...state};
     }
+//     if (action.obj === '1' && action.diya === '+') {
+//         return {...state, a: state.a + 5};
+//     } else if (action.obj === '1' && action.diya === '-') {
+//         return {...state, a: state.a - 5};
+//     }
+//
+//     if (action.obj === '2' && action.diya === '+') {
+//         return {...state, b: state.b + 10};
+//     } else if (action.obj === '2' && action.diya === '-') {
+//         return {...state, b: state.b - 10};
+//     }
+//
+//     if (action.obj === '3' && action.diya === '+') {
+//         return {...state, c: state.c ++};
+//     } else if (action.obj === '3' && action.diya === '-') {
+//         return {...state, c: state.c - 1};
+//     }
 
-    if (action.obj === '3' && action.diya === '+') {
-        return {...state, c: state.c ++};
-    } else if (action.obj === '3' && action.diya === '-') {
-        return {...state, c: state.c - 1};
-    }
-
-    return {...state};
+    // return {...state};
 
 }
 
 export default function App() {
-
     let [state, dispatch] = useReducer(reduser, {a: 0, b: 0, c: 0});
     let {a, b, c} = state;
 
@@ -40,18 +47,19 @@ export default function App() {
         <div className={'buttons'}>
             <div className={'button'}>
                 <h1>Object a = {a} </h1>
-                <button onClick={() => dispatch({obj: '1', diya: '+'})}>increment on +5</button>
-                <button onClick={() => dispatch({obj: '1', diya: '-'})}>decrement on -5</button>
+                <button onClick={() => dispatch({obj: 'a', count: 5, diya: '+'})}>increment on +5</button>
+                <button onClick={() => dispatch({obj: 'a', count: 5, diya: '-'})}>decrement on -5</button>
+
             </div>
             <div className={'button'}>
                 <h1>Object b = {b} </h1>
-                <button onClick={() => dispatch({obj: '2', diya: '+'})}>increment on +10</button>
-                <button onClick={() => dispatch({obj: '2', diya: '-'})}>decrement on -10</button>
+                <button onClick={() => dispatch({obj: 'b', count: 10,   diya: '+'})}>increment on +10</button>
+                <button onClick={() => dispatch({obj: 'b', count: 10,  diya: '-'})}>decrement on -10</button>
             </div>
             <div className={'button'}>
                 <h1>Object c = {c} </h1>
-                <button onClick={() => dispatch({obj: '3', diya: '+'})}>increment on +1</button>
-                <button onClick={() => dispatch({obj: '3', diya: '-'})}>decrement on -1</button>
+                <button onClick={() => dispatch({obj: 'c', count: 1,  diya: '+'})}>increment on +1</button>
+                <button onClick={() => dispatch({obj: 'c', count: 1,  diya: '-'})}>decrement on -1</button>
             </div>
         </div>
     );
