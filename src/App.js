89bc8fa18cf9f02836ab7c5422065c1242fc046d
learn13579 <maryana.poshta@gmail.com>
2021-08-31 16:33:33 +0300

    redux 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,47 @@
 import './App.css';
+import {useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+
+const CreateTodoForm = () => {
+    const [title, setTitle]=useState('');
+    const [description, setDescription]=useState('');
+
+    return (
+        <div>
+            <form onSubmit={()=>{
+                console.log('submite')
+            }}>
+                <input type={'text'} value={title} onChange={({target:{value}})=>setTitle((value)) } placeholder={"todo title"}/>
+                <br/>
+                {/*<input type={'text'} value={description} onChange={} placeholder={"todo description"}/>*/}
+                <br/>
+                <button type="submit">create todo</button>
+            </form>
+        </div>
+    )
+
+}
+
+const Todos = () => {
+
+}
 
 function App() {
+    const store = useSelector(store => store);
+
+    console.log(store);
+
+
+    const fetchTodos = async () => {
+        const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+        // const data = await
+    }
+
+    useEffect(() => {
+        fetchTodos()
+    }, [])
+
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux";
+import {createStore} from "redux";
+import {LOAD_USERS, ADD_USER} from "./redux/actions";
+import {addUser, loadUsers} from "./redux/actionCreators";
+
+let initialState = {};
+const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case LOAD_USERS:
+            return {...state, users: [action.payload]}
+
+        case ADD_USER:
+            let newUser = action.payload;
+            let newUserArray = [...state.users, newUser];
+            return {...state, users: [...newUserArray] }
+        default:
+            return state;
+    }
+}
+
+let store = createStore(reducer);
+store.dispatch(loadUsers([]));
+store.dispatch(addUser({}));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
